0df38e9187c73c120ef6fae921d54e496a14a386
Rafael Goulart <faelplg@gmail.com>
2020-03-28 23:30:27 -0300

    Fix: Adjust footer responsivity with the columns text.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-console.log("Copyright by faelplg. You are 100% allowed to use this webpage for both personal and commercial use, but NOT to claim it as your own design. A credit to the original author, Rafael Goulart, is of course highly appreciated!");
+console.log("%c Copyright by faelplg. You are 100% allowed to use this webpage for both personal and commercial use, but NOT to claim it as your own design. A credit to the original author, Rafael Goulart, is of course highly appreciated!", "color: #21eb29; background: #222; padding: 4px;");
 
 particlesJS.load("particles-js", "./particles.json", function() {
-    console.log("callback - particles.js config loaded");
+    console.log("Particles successfully loaded.");
 });
